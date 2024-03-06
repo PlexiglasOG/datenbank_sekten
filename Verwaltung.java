fef7a59987b10c95f9cef0ca8b7014c0080cce16
Konrad Sonntag <konrad@sonntagsbox.de>
2024-03-06 11:04:00 +0100

    Hallo

--- Verwaltung.java
+++ Verwaltung.java
@@ -1,50 +1,35 @@
 
 public class Verwaltung {
 
-    DatabaseConnector meinConnector;
+    DatabaseConnector connector;
 
     public Verwaltung(){
-        /**Ein Objekt der Klasse DatabaseConnector wird erstellt.
-         *SQLite verlangt als einzige Angabe den Namen der Datei!
-         *Die anderen Angaben sind irrelevant.
-         */  
-        meinConnector = new DatabaseConnector("", 0, "Schuldaten.db", "", "");
+       connector = new DatabaseConnector("", 0, "Schuldaten.db", "", "");
     }
 
     public void aktuelleFehlermeldung(){
-        /**
-         *Zur Sicherheit sollten wir sehr häufig die Fehlermeldungen der Datenbank prüfen.
-         */
-        System.out.println(meinConnector.getErrorMessage());
-        /**
-         *Das Ergebnis wird auf der Konsole angezeigt.
-         */
+        System.out.println(connector.getErrorMessage());
     }
 
     public void sqlBefehlAusfuehren(String sqlBefehl){
-        meinConnector.executeStatement(sqlBefehl);  //Die Methode executeStatement der Klasse DatabaseConnector wird ausgeführt. Der sql-Befehl wird als String übergeben.
+        connector.executeStatement(sqlBefehl);  //Die Methode executeStatement der Klasse DatabaseConnector wird ausgeführt. Der sql-Befehl wird als String übergeben.
 
-        for (int i=0; i<meinConnector.getCurrentQueryResult().getRowCount(); i=i+1) {               //verschachtelte for-Schleifen, die das 'QueryResult' auslesen.
+        for (int i=0; i<connector.getCurrentQueryResult().getRowCount(); i=i+1) {               //verschachtelte for-Schleifen, die das 'QueryResult' auslesen.
 
-            for (int j=0; j<meinConnector.getCurrentQueryResult().getColumnCount(); j=j+1) {
-                System.out.print(meinConnector.getCurrentQueryResult().getData()[i][j]+" ");
+            for (int j=0; j<connector.getCurrentQueryResult().getColumnCount(); j=j+1) {
+                System.out.print(connector.getCurrentQueryResult().getData()[i][j]+" ");
 
             }
             System.out.println();
         }
     }
 
-    public void alleSchuelerinnenAusgeben(){        //Nur, um zu sehen, wie es geht...
+    public void alleSchuelerinnenAusgeben(){       
         String auftrag="select * from Schueler where Geschlecht='w'";
         sqlBefehlAusfuehren(auftrag);
     }
 
-    /**
-     * Alle nicht implementierten Methoden müssen geschrieben werden!
-     */
-
-    public void alleSchuelerAusgeben(){         //diesmal männlich
-
+    public void alleSchuelerAusgeben(){     
     }
 
     public void alleSchuelerNichtAusHanauAusgeben(){
@@ -66,14 +51,14 @@ public class Verwaltung {
     public void schuelerEinfuegenTest()      //Nur, um zu sehen, wie es geht...
     {
         String auftrag="INSERT INTO 'Schueler' ('SID', 'Vorname', 'Name', 'Geburtsdatum', 'Geschlecht','Wohnort','PLZ','Strasse','Hausnummer','Bezeichnung') VALUES (NULL, 'Felix', 'Finster', '2000-02-14','m','Bonn', 53177,'Siebenmorgenweg',34,'Q1')";
-        meinConnector.executeStatement(auftrag);
+        connector.executeStatement(auftrag);
         aktuelleFehlermeldung();
     }
 
     public void schuelerEinfuegen(String pV, String pN, String pGD, String pG, String pW, int pLZ, String pS,int pH, String pK )     //Alle Parameter übergeben
     {
         String auftrag="INSERT INTO 'Schueler' ('SID', 'Vorname', 'Name', 'Geburtsdatum', 'Geschlecht','Wohnort','PLZ','Strasse','Hausnummer','Bezeichnung') VALUES (NULL, '"+pV+"', '"+pN+"', '"+pGD+"','"+pG+"','"+pW+"', "+pLZ+",'"+pS+"',"+pH+",'"+pK+"')";
-        meinConnector.executeStatement(auftrag);
+        connector.executeStatement(auftrag);
         aktuelleFehlermeldung();
     }
 
