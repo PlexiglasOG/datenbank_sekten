9d8b549d618ea24cc73aa9d9650643d597583704
PlexiglasOG <vakoum2018@gmail.com>
2024-03-13 10:57:43 +0100

    fdf

--- Verwaltung.java
+++ Verwaltung.java
@@ -72,7 +72,6 @@ public class Verwaltung {
         aktuelleFehlermeldung();
     }
     
-
     public void mitgliedEinzufuegen(int pSEHAID, String pVorname, String pName, String pGeburtsdatum, String pBekehrungsdatum, String pSterbedatum){
         String auftrag="INSERT INTO 'Mitglied' ('MIID', 'SEHAID', 'Vorname', 'Name', 'Geburtsdatum','Bekehrungsdatum','Sterbedatum') VALUES (NULL, '"+pSEHAID+"', '"+pVorname+"', '"+pName+"', '"+pGeburtsdatum+"','"+pBekehrungsdatum+"','"+pSterbedatum+"')";
         connector.executeStatement(auftrag);
